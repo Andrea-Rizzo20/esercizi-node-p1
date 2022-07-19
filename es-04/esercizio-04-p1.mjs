52362af4784430d8d77cdeccb2f2065c58da68e1
Andrea Rizzo <r.andrea2093@gmail.com>
2022-07-19 13:46:03 +0200

    esercizio-04

---
+++ es-04/esercizio-04-p1.mjs
@@ -0,0 +1,5 @@
+function consoleThis(par){
+    console.log(`ecco ancora ${par}`)
+}
+
+export default consoleThis
---
+++ es-04/esercizio-04-p2.mjs
@@ -0,0 +1,3 @@
+import consoleThis from "./esercizio-04-p1.mjs";
+
+consoleThis('Andrea')

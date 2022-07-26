da921bd774043042a6772c54dde12320796f086a
Andrea Rizzo <r.andrea2093@gmail.com>
2022-07-26 12:32:07 +0200

    esercizio-07

---
+++ es-07/es-07.mjs
@@ -0,0 +1,9 @@
+import figlet from 'figlet'
+
+console.log(figlet.textSync('Ciao Andrea', {
+    font: 'Merlin1',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}));
